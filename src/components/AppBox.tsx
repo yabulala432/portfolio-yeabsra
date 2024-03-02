db6044a7d8433d7ae754bd956a48d0affdc98cb2
Yeabsira Yonas <yabulala432@gmail.com>
2024-03-02 06:10:42 +0300

    make some changes on app box component

--- src/components/AppBox.tsx
+++ src/components/AppBox.tsx
@@ -3,29 +3,32 @@ import { Box } from "@chakra-ui/react";
 
 interface props {
   children: React.ReactNode;
+  props?: any;
 }
 
-function AppBox({ children }: props) {
+function AppBox({ children, props }: props) {
   return (
     <Box
-      bgColor={"#D0722F"}
+      bgColor={"#2c2c6c"}
       borderRadius={"5%"}
       h={"100%"}
       w={"100%"}
       p={5}
       display={"flex"}
+      flexDirection={"column"}
       alignItems={"center"}
       justifyContent={"center"}
       color={"white"}
-      boxShadow={"0 0 1px 1px #FFF"}
+      // boxShadow={"0 0 1px 1px #FFF"}
       transition={"all 1s ease-in-out"}
       _hover={{
-        boxShadow: "0 0 1px 1px #FFF",
+        boxShadow: "0 0 0px 1px #FFF",
         transition: "all 1.5s ease-in-out",
         bgColor: "transparent",
-        border: "2px solid #FFF",
+        border: "1px solid #FFF",
         transform: "scale(1.1)",
       }}
+      {...props}
     >
       {children}
     </Box>
